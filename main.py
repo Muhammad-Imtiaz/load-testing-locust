75dac81deda60815da1430e5a91b4f9dba320f6d
Muhammad Imtiaz <muhammad.imtiaz@emumba.com>
2021-03-17 19:28:40 +0500

    Added locust load test script for Rainfocus apis

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+from locust import (HttpUser, task, between, TaskSet)
+
+BASE_URL = 'https://api.fankave.com'
+HEADERS = {'Content-type': 'application/json', 'clientapikey': 'K5MeJJ3eQmZWt52K'}
+
+@task()
+def polling_rainfocus_api(self):
+
+    # tokens list
+    token_list = ['2dbb20df40e54b8189faed7229d2610b']
+
+    for token in token_list:
+        headers = {"Authorization": "Bearer " + token}
+        headers.update(HEADERS)
+        with self.client.post(f'{BASE_URL}/verifyRainfocus', headers=headers, catch_response=True) as response:
+            if response.text != "Success":
+                return response.failure(f"Unable to communicate to rainfocus")
+
+class IdsApp(HttpUser):
+    tasks = [polling_rainfocus_api]
+
+
